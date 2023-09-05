1153ddd4df975d5d43f6999fc5b890a66ae0a5d1
aoncodev <aoncodev@gmail.com>
2023-09-06 08:47:42 +0900

    Fix migrations

--- employee_attendance/employee_attendance/settings.py
+++ employee_attendance/employee_attendance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&_exjp$5i+q621&6*ex#^2rh29@rfcormf6_i!b@rbg$=u*a7!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.78.236.151']
+ALLOWED_HOSTS = ['52.78.236.151', 'localhost']
 
 
 # Application definition
