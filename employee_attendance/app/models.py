dc74ba695c06e0bfb15d8405119a926c314aabf9
aoncodev <aoncodev@gmail.com>
2023-09-06 06:36:46 +0900

    Adding User and ClockEntry models

--- employee_attendance/app/models.py
+++ employee_attendance/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    pin = models.CharField(max_length=4)
+    hourly_wage = models.DecimalField(max_digits=10, decimal_places=2)
+
+class ClockEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    clock_in = models.DateTimeField()
+    clock_out = models.DateTimeField(null=True, blank=True)
+    wage = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
